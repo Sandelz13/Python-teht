b59a968b888895287b263b1e300fef8013a868b7
Sandelz13 <122279815+Sandelz13@users.noreply.github.com>
2023-01-13 06:46:41 +0200

    1st module greeting

--- pythonProject/2/2/__init__.py
+++ pythonProject/2/2/__init__.py
@@ -5,6 +5,6 @@ print (math.pi)
 M=math
 print()
 
-Säde = float (input ("Kerro minulle ympyrän säde"))
+Säde = float (input ("Kerro minulle ympyrän säde "))
 pinta_ala = M.pi* Säde * Säde
 print (" Ympyrän pinta-ala on: ", pinta_ala)
